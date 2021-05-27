1c2ffa2a77608df37f9df1c5513fef6767214f4c
Danilo Vilhena <danilo.vilhena@dellead.com>
2021-05-27 15:33:35 -0300

    Inclusão de GH Pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/calculadora-acertos",
   siteMetadata: {
     title: `Calculadora de Acertos`,
     description: `project to calculate the percentage of correct answers in a number of questions.`,
@@ -29,8 +30,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`
   ],
 }
