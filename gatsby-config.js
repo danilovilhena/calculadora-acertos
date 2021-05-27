886a6b973791dc36c138799e4750f8252dbe45b3
Danilo Vilhena <danilo.vilhena@dellead.com>
2021-05-27 15:31:52 -0300

    Lógica do sistema e ajustes gerais

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Calculadora de Acertos`,
+    description: `project to calculate the percentage of correct answers in a number of questions.`,
+    author: `@danilovilhena`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
     `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-source-filesystem`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,57 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import { useState } from "react"
 import "../styles/index.css"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  const [result, setResult] = useState('')
+  const [total, setTotal] = useState(0)
+  const [answers, setAnswers] = useState(0)
+
+  const handleTotal = event => {setTotal(event.target.value)}
+  const handleAnswers = event => {setAnswers(event.target.value)}
+  const handleResult = () => {
+    let resultString = ((answers/total)*100).toFixed(1)
+
+    setResult(resultString)
+    console.log(resultString > 75)
+  }
+  
+  return (
+  <main className="mx-auto my-2">
+    <h1>Calculadora de Acertos</h1>
+    <section className="mt-4 shadow-md">
+      <div className="mb-6">
+        <label htmlFor="total">Qual a quantidade <b>total</b> de questões?</label>
+        <input id="total" className="shadow-sm" type="number" min="0" max="999" value={total} onChange={handleTotal}></input>
+      </div>
+      <div className="mb-6">
+        <label htmlFor="answers">Quantas questões <b>você acertou</b>?</label>
+        <input id="answers" className="shadow-sm" type="number" min="0" max={total} value={answers} onChange={handleAnswers}></input>
+      </div>
+      <button onClick={() => {handleResult()}}>Calcular resultado 🔥</button>
+      {(result >= 100) && (
+        <div className="alert-yellow mt-6">
+          <p>😬 Esses valores não deram um resultado válido, tente digitar novamente!</p>
+        </div>
+      )}
+      {(result >= 75 && result < 100 && result != 0) && (
+        <div className="alert-green mt-6">
+          <p>😄 Sensacional, seu resultado foi: <b>{result}%</b></p>
+        </div>
+      )}
+      {(result >=50 && result < 75 && result != 0) && (
+        <div className="alert-yellow mt-6">
+          <p>😀 Muito bem, seu resultado foi: <b>{result}%</b></p>
+        </div>
+      )}
+      {(result < 50 && result != 0) && (
+        <div className="alert-orange mt-6">
+          <p>🙂 Não desista, seu resultado foi: <b>{result}%</b></p>
+        </div>
+      )}
+      
+    </section>
+  </main>
+)}
 
 export default IndexPage
